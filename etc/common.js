d30a85892df301285701af0ab742047e58507cc4
macmoo <hscrollhooli@gmail.com>
2021-07-22 14:40:29 +0900

    0722

--- etc/common.js
+++ etc/common.js
@@ -1,20 +1,3 @@
-// var coll = document.getElementsByClassName("collapsible");
-// var i;
-// for (i = 0; i < coll.length; i++) {
-//     coll[i].addEventListener("click", function () {
-//         this.classList.toggle("active");
-//         var content = this.nextElementSibling;
-//         if (content.style.maxHeight) {
-//             alert('1' + this);
-//             alert('2' + content.style.maxHeight);
-//             content.style.maxHeight = null;
-//         } else {
-//             alert('3' + this);
-//             alert('4' + content.style.maxHeight);
-//             content.style.maxHeight = content.scrollHeight + "px";
-//         }
-//     });
-// }
 var coll = document.getElementsByClassName("collapsible");
 var i;
 for (i = 0; i < coll.length; i++) {
