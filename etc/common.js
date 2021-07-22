668f3aec40639227c3323542a7e8b7fabc403858
macmoo <hscrollhooli@gmail.com>
2021-07-22 14:24:38 +0900

    0722

--- etc/common.js
+++ etc/common.js
@@ -5,9 +5,17 @@ for (i = 0; i < coll.length; i++) {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
         if (content.style.maxHeight) {
+            alert('1' + this);
+            alert('2' + content.style.maxHeight);
             content.style.maxHeight = null;
         } else {
+            alert('3' + this);
+            alert('4' + content.style.maxHeight);
             content.style.maxHeight = content.scrollHeight + "px";
         }
     });
 }
+
+// $('.content').click(function(){
+//     alert('123');
+// });
