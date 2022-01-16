59ac3f18804b628cc7164121ca5a29eb4245039f
macmoo <hscrollhooli@gmail.com>
2022-01-16 21:28:35 +0900

    220116

--- _test/002.js
+++ _test/002.js
@@ -1,9 +1,3 @@
-// const로 정수를
-// let로   변수를 취급
-let   myName1 = "이름1";
-const myName2 = "이름2";
-
-// myName2 = "이름3"; // 에러
-
-alert(myName1);
-alert(myName2);
+// ----------------------------
+// 애로우함수 (=>)
+// 
