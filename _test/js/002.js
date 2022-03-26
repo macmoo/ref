a3c5ad3b82a8992bbb359e4df37c965254d011e8
macmoo <hscrollhooli@gmail.com>
2022-03-26 17:42:48 +0900

    220326

--- _test/js/002.js
+++ _test/js/002.js
@@ -1,28 +1,10 @@
 // ----------------------------
-// - 자이로 센서, 가속도 센서 사용
-//   : event.beta  => x축의 기울기
-//     event.gamma => y축의 기울기
-//     event.alpha => z축의 기울기
-//   : event.acceleration.x => x축의 가속도
-//     event.acceleration.y => y축의 가속도
-//     event.acceleration.z => z축의 가속도
-//   : 자이로 센서는 기울기를 검출하는 센서.
-//     DeviceOrientation이벤트로 검출.
+// - 진동
 // ----------
-window.addEventListener('deviceorientation', deviceOrientationHandler);
-function deviceOrientationHandler(event){
-    const beta  = evnet.beta;  // x축
-    const gamma = evnet.gamma; // y축
-    const alpha = evnet.alpha; // z축
-
-    alert(`X축 : ${beta}\n`
-        + `Y축 : ${gamma}\n`
-        + `Z축 : ${alpha}\n`);
-}
-// ----------------------------
-// - 
-// ----------
-// ----------------------------
+// 1000미리초 진동
+navigator.vibrate(1000);
+// 500진동, 100정지, 그후에 500진동
+navigator.vibrate([500,100,500]);
 // - 
 // ----------
 // ----------------------------
---
+++ f/7phone/003_vib.js
@@ -0,0 +1,1 @@
+navigator.vibrate([500,2000,5000]);
